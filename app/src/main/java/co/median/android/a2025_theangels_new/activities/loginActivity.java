1e82056b0c91c29e58d7317e24b13e6c56817e46
Omer Gamliel <omergamliel@gmail.com>
2025-03-02 23:17:54 +0200

    update
    update

--- app/src/main/java/co/median/android/a2025_theangels_new/activities/loginActivity.java
+++ app/src/main/java/co/median/android/a2025_theangels_new/activities/loginActivity.java
@@ -1,7 +1,6 @@
 package co.median.android.a2025_theangels_new.activities;
 
 import android.os.Bundle;
-
 import co.median.android.a2025_theangels_new.R;
 
 public class loginActivity extends BaseActivity {
@@ -9,8 +8,6 @@ public class loginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginactivity); // קישור ל-XML של המסך
-
         // קריאה לפונקציה מהמחלקה הראשית שמחברת את ה-HEADER וה-FOOTER
         setupHeaderAndFooter();
 
